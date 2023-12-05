61fc7740268c425b6315af83b2b4804b2ea45ed2
Christian <33626162+Kiichen@users.noreply.github.com>
2023-12-05 12:11:42 +0100

    almanac

---
+++ 2023/5/almanac.py
@@ -0,0 +1,76 @@
+input ="""
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
+
+def get_groups(lines: str, start: str, end: str) -> list[str]:
+    return lines[lines.find(start)+len(start):lines.rfind(end)].strip().splitlines()
+
+def convert_groups_to_map(groups: list[str]) -> dict[int, int]:
+    group_dict = {}
+    for group in groups:
+        destination, source, length = map(int, group.split())
+        for i in range(length):
+            group_dict.setdefault(source + i, destination + i)
+    return group_dict
+
+def get_next_group(current: list[int], group: dict[int, int]) -> list[int]:
+    return [group.get(item, item) for item in current]
+
+def handle_group(lines: str, prev: list[int], start: str, end: str) -> list[str]:
+    groups = get_groups(lines, start, end)
+    group_dict = convert_groups_to_map(groups)
+    return get_next_group(prev, group_dict)
+
+def get_lowest_location(input: str):
+    lines = input.strip()
+
+    seeds = get_groups(lines, "seeds:", "seed-to-soil map:")[0].split(" ")
+    seeds = list(map(int, seeds))
+
+    soils = handle_group(lines, seeds, "seed-to-soil map:", "soil-to-fertilizer map:")
+    fertilizers = handle_group(lines, soils, "soil-to-fertilizer map:", "fertilizer-to-water map:")
+    waters = handle_group(lines, fertilizers, "fertilizer-to-water map:", "water-to-light map:")
+    lights = handle_group(lines, waters, "water-to-light map:", "light-to-temperature map:")
+    temperatures = handle_group(lines, lights, "light-to-temperature map:", "temperature-to-humidity map:")
+    humidities = handle_group(lines, temperatures, "temperature-to-humidity map:", "humidity-to-location map:")
+    locations = handle_group(lines, humidities, "humidity-to-location map:", "")
+    
+    return min(locations)
+
+
+assert get_lowest_location(input) == 35
+
+with open("2023/5/input.txt") as f:
+    print(get_lowest_location(f.read()))
